e5beac0e7de34f163094b2ff54265469ef563eec
Kratoo5 <kratos666666@gmail.com>
2015-11-03 20:13:38 +0200

    selectable dog.

---
+++ Assets/Scripts/ControlledScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ControlledScript : MonoBehaviour
+{
+    static readonly Vector3 idleVector = Vector3.one;
+
+    enum Directions
+    {
+        Left = -1,
+        Right = 1,
+        Up = 2,
+        Down = -2,
+        Idle = 0
+    }
+
+    Vector3 startPos, targetPos;
+    Animator animator;
+
+
+    public float speed = 10f;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        startPos = transform.position;
+        targetPos = idleVector;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetPos != idleVector)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
+            if (transform.position == targetPos)
+            {
+                StopMoving();
+            }
+        }
+    }
+
+    internal void MoveTo(Vector2 newPosition)
+    {
+        startPos = transform.position;
+
+        bool isMoving = targetPos != idleVector;
+        targetPos = new Vector3(newPosition.x, newPosition.y, transform.position.z);
+
+        float deltaX = targetPos.x - transform.position.x;
+        float deltaY = targetPos.y - transform.position.y;
+
+        int direction = (int)GetDirection(deltaX, deltaY);
+        if (!isMoving || direction != animator.GetInteger("direction") )
+        {
+            animator.SetInteger("direction", direction);
+            animator.SetTrigger("move");
+        }
+    }
+
+    void StopMoving()
+    {
+        targetPos = idleVector;
+        animator.SetTrigger("stop");
+    }
+
+
+    private Directions GetDirection(float dX, float dY)
+    {
+        if (Mathf.Abs(dX) > Mathf.Abs(dY))
+        {
+            if (dX > 0)
+                return Directions.Right;
+            else
+                return Directions.Left;
+        }
+        else
+        {
+            if (dY > 0)
+                return Directions.Up;
+            else
+                return Directions.Down;
+        }
+    }
+}
---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour
+{
+
+    public ControlledScript currentDog;
+    Vector2 newPosition;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                ControlledScript selectedDog = hit.collider.gameObject.GetComponent<ControlledScript>();
+                if (selectedDog!=null)
+                {
+                    currentDog = selectedDog;
+                    newPosition = Vector2.zero;
+                }
+            }
+
+            else if (currentDog != null)
+            {
+                newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                currentDog.MoveTo(newPosition);
+            }
+        }
+    }
+}
