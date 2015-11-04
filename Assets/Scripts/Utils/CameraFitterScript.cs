af84f2b50591b6a5a7730e0862f5955674523570
Kratoo5 <kratos666666@gmail.com>
2015-11-04 03:20:34 +0200

    camera fitting, only landscape.

---
+++ Assets/Scripts/Utils/CameraFitterScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFitterScript : MonoBehaviour {
+
+    public SpriteRenderer field;
+
+    const float targetAspect = 1920f / 1080f;
+	// Use this for initialization
+	void Start () {
+
+        //all we need - is to whole field to be on the screen
+        if (Camera.main.aspect < targetAspect)
+        {
+           Camera.main.orthographicSize = field.bounds.size.x * Screen.height / Screen.width * 0.5f;
+        }
+	}
+}
